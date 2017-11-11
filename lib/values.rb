8e3e53b3c331d5cc32a34fe4b93ac008946a37e3
Sherry <sherryptk@gmail.com>
2017-11-11 16:56:45 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -11,6 +11,6 @@ require 'pry'
 def get_the_values(groceries)
   #code your solution here!
   collection = []
-    collection << groceries.value
+    collection << groceries.values
   collection
 end
